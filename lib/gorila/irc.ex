bae97455fddea3a01c67fc3072fc474c8fd5ae5a
Kore Nordmann <mail@kore-nordmann.de>
2013-08-06 13:12:43 +0200

    Implemented & Tested: Parse IRC message

---
+++ lib/gorila/irc.ex
@@ -0,0 +1,37 @@
+defmodule Gorila.Irc do
+
+  def parse(string) when is_binary(string) do
+    [_all, server, command, params, text] = Regex.run(
+      %r/\A(?::(\S+)\s*)?([A-Za-z]+|\d{3})((?:\s+[^:\s]\S*)*)(?:\s+:(.*))?\s*?$/m,
+      string
+    )
+    message = Gorila.Irc.Message.new(
+      command: command,
+      params: String.split(params),
+      text: String.strip(text)
+    )
+    parseServer(message, server)
+  end
+
+  defp parseServer(message, server) do
+    {_, pattern} = Regex.compile("^(\\S*)!(\\S*)@(\\S*)$")
+    cond do
+      Regex.match?(pattern, server) ->
+        [_all, nick, ident, host] = Regex.run(pattern, server)
+        message = message.nick(nick)
+        message = message.ident(ident)
+        message = message.host(host)
+      true ->
+        message = message.server(server)
+    end
+  end
+
+  defrecord Message,
+    server: nil,
+    nick: nil,
+    ident: nil,
+    host: nil,
+    command: nil,
+    params: [],
+    text: nil
+end
---
+++ test/gorila/irc_test.exs
@@ -0,0 +1,15 @@
+defmodule GorilaIrcTest do
+  use ExUnit.Case
+
+  test "Parse simple IRC message" do
+    assert(
+      Gorila.Irc.Message.new(
+        server: "",
+        command: "SERVER",
+        params: ["test.oulu.fi", "1"],
+        text: "[tolsun.oulu.fi] Experimental server"
+      ) ==
+      Gorila.Irc.parse("SERVER test.oulu.fi 1 :[tolsun.oulu.fi] Experimental server\r\n")
+    )
+  end
+end
